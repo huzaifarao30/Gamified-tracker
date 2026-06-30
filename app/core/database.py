84e797a0289e454b87a1924add93a8f5b96875dc
huzaifarao30 <huzaifarao727@gmail.com>
2026-06-30 16:08:34 +0500

    Updated routes logic frm fake db to mongo db

--- app/core/database.py
+++ app/core/database.py
@@ -6,8 +6,12 @@ load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
 
+if not MONGO_URI:
+    raise ValueError("MONGO_URI is not set! Check your .env file.")
+
 try:
     client = MongoClient(MONGO_URI)
+    client.admin.command('ping')
     print("Successfully connected to database")
 except Exception as e:
     print(f"Failed to connect to database : {e}")
